f871686c2781a5cd93526141e5d60bb974d8b1e6
chandan <chandanmahato624@gmail.com>
2023-12-03 11:27:37 +0530

    Added all assets

--- shopping_app/lib/main.dart
+++ shopping_app/lib/main.dart
@@ -1,22 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/app.dart';
 
 void main() {
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(fontSize: 14, color: Colors.green),
-        ),
-      ),
-      darkTheme: ThemeData(),
-    );
-  }
 }
