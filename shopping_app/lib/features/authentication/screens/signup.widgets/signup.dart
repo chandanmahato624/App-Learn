9db5100942884dd946c58dad749ba1072009b3f9
chandan <chandanmahato624@gmail.com>
2023-12-09 22:01:54 +0530

    Day-9 SignUp Page Complete

--- shopping_app/lib/features/authentication/screens/signup.widgets/signup.dart
+++ shopping_app/lib/features/authentication/screens/signup.widgets/signup.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_utils/get_utils.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:shopping_app/common/widgets_login_signup/form_divider.dart';
+import 'package:shopping_app/common/widgets_login_signup/social_buttons.dart';
+import 'package:shopping_app/features/authentication/screens/signup.widgets/widget/signup_from.dart';
+import 'package:shopping_app/utils/constants/colors.dart';
 import 'package:shopping_app/utils/constants/sizes.dart';
 import 'package:shopping_app/utils/constants/text_strings.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
@@ -14,6 +20,7 @@ class SignupScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(TSizes.defaultSpace),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               /// Title
               Text(TTexts.signupTitle,
@@ -21,73 +28,15 @@ class SignupScreen extends StatelessWidget {
               const SizedBox(height: TSizes.spaceBtwSections),
 
               /// Form
-              Form(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            expands: false,
-                            decoration: const InputDecoration(
-                                labelText: TTexts.firstName,
-                                prefixIcon: Icon(Iconsax.user)),
-                          ),
-                        ),
-                        const SizedBox(width: TSizes.spaceBtwInputFields),
-                        Expanded(
-                          child: TextFormField(
-                            expands: false,
-                            decoration: const InputDecoration(
-                                labelText: TTexts.lastName,
-                                prefixIcon: Icon(Iconsax.user)),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
+              const TSignUpFrom(),
+              const SizedBox(height: TSizes.spaceBtwSections),
 
-                    /// User Name
-                    TextFormField(
-                      expands: false,
-                      decoration: const InputDecoration(
-                          labelText: TTexts.username,
-                          prefixIcon: Icon(Iconsax.user_edit)),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
+              /// Divider
+              TFormDivider(dividerText: TTexts.orSignUpWith.capitalize!),
+              const SizedBox(height: TSizes.spaceBtwSections),
 
-                    /// Email
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          labelText: TTexts.email,
-                          prefixIcon: Icon(Iconsax.direct)),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                    /// Phone number
-                    TextFormField(
-                      decoration: const InputDecoration(
-                          labelText: TTexts.phoneNo,
-                          prefixIcon: Icon(Iconsax.call)),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                    /// Password
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: TTexts.password,
-                        prefixIcon: Icon(Iconsax.password_check),
-                        suffixIcon: Icon(Iconsax.eye_slash),
-                      ),
-                    ),
-                    const SizedBox(height: TSizes.spaceBtwInputFields),
-
-                    /// Terms and condition
-                    /// SignUpButton
-                  ],
-                ),
-              ),
+              /// Social buttons
+              const TSocialButtons(),
             ],
           ),
         ),
