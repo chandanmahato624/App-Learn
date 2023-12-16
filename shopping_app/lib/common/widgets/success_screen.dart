1aed820fae6ab520bf937d24652299e3c3d281eb
chandan <chandanmahato624@gmail.com>
2023-12-16 23:45:54 +0530

    Half of Success screen

---
+++ shopping_app/lib/common/widgets/success_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/common/styles/spacing_styles.dart';
+import 'package:shopping_app/utils/constants/image_strings.dart';
+import 'package:shopping_app/utils/constants/sizes.dart';
+import 'package:shopping_app/utils/constants/text_strings.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBerHeight * 2,
+          child: Column(children: [
+            Image(
+              image: const AssetImage(TImages.deliveredEmailIllustration),
+              width: THelperFunctions.screenWidth() * 0.6,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            ///Title and subtitle
+            Text(
+              TTexts.yourAccountCreatedTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              'mail.chandanmahato@gmail.com',
+              style: Theme.of(context).textTheme.labelLarge,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: TSizes.spaceBtwItems),
+            Text(
+              TTexts.yourAccountCreatedSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: TSizes.spaceBtwSections),
+
+            ///Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: const Text(TTexts.tContinue),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
