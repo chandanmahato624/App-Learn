43fd1c6e947075b5366622885571472a8ba5eebc
chandan <chandanmahato624@gmail.com>
2023-12-25 19:26:39 +0530

    Home app bar completed with shopping icon

--- shopping_app/lib/features/shop/screens/home/home.dart
+++ shopping_app/lib/features/shop/screens/home/home.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:shopping_app/features/shop/screens/home/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-              child: Container(),
-            ),
+                child: Column(
+              children: [THomeAppBar()],
+            )),
           ],
         ),
       ),
