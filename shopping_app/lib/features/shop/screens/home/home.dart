55dfc51053fec7f999559e4d537f36b6d13cfbc7
chandan <chandanmahato624@gmail.com>
2023-12-26 19:04:36 +0530

    Serch container completed

--- shopping_app/lib/features/shop/screens/home/home.dart
+++ shopping_app/lib/features/shop/screens/home/home.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/container/primary_header_container.dart';
+import 'package:shopping_app/common/widgets/custom_shapes/container/search_container.dart';
 import 'package:shopping_app/features/shop/screens/home/home_appbar.dart';
+import 'package:shopping_app/utils/constants/colors.dart';
+import 'package:shopping_app/utils/constants/sizes.dart';
+import 'package:shopping_app/utils/device/device_utility.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,7 +19,14 @@ class HomeScreen extends StatelessWidget {
           children: [
             TPrimaryHeaderContainer(
                 child: Column(
-              children: [THomeAppBar()],
+              children: [
+                /// --- AppBar ---
+                THomeAppBar(),
+                SizedBox(height: TSizes.spaceBtwSections),
+
+                /// --- SearchBar ---
+                TSearchContainer(text: 'Search in Store'),
+              ],
             )),
           ],
         ),
