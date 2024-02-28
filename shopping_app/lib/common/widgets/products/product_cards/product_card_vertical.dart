fe71acdcab0d37c02ba14ac4ce8fb2146e6bf95a
chandan <chandanmahato624@gmail.com>
2024-02-28 15:30:01 +0530

    Update previues works

---
+++ shopping_app/lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TProductCardVertical extends StatelessWidget {
+  const TProductCardVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 180,
+      padding: const EdgeInsets.all(1),
+      decoration: BoxDecoration(
+        boxShadow:[TShadowStyle.] ),
+    );
+  }
+}
