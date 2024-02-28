e132f07a4a9c88da27eda9b1605ab0cc5b1af7ae
chandan <chandanmahato624@gmail.com>
2024-02-28 21:56:43 +0530

    round container complete

--- shopping_app/lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ shopping_app/lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/common/styles/shadows.dart';
+import 'package:shopping_app/utils/constants/colors.dart';
+import 'package:shopping_app/utils/constants/sizes.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
 class TProductCardVertical extends StatelessWidget {
   const TProductCardVertical({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Container(
       width: 180,
       padding: const EdgeInsets.all(1),
       decoration: BoxDecoration(
-        boxShadow:[TShadowStyle.] ),
+        boxShadow: [TShadowStyle.verticalProductShadow],
+        borderRadius: BorderRadius.circular(TSizes.productImageRadius),
+        color: dark ? TColors.darkGrey : TColors.white,
+      ),
+      child: Column(
+        children: [
+          TRoundedContainer{
+            
+          }
+        ],
+      ),
     );
   }
 }
