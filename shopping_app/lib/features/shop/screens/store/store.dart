b1d04bfaf89031d4741c3ba9989345bc08141f9c
chandan <chandanmahato624@gmail.com>
2024-03-09 10:54:40 +0530

    Store Search section done

--- shopping_app/lib/features/shop/screens/store/store.dart
+++ shopping_app/lib/features/shop/screens/store/store.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/common/widgets/appbar/appbar.dart';
+import 'package:shopping_app/common/widgets/custom_shapes/container/search_container.dart';
 import 'package:shopping_app/common/widgets/products.cart/cart_menu_icon.dart';
 import 'package:shopping_app/utils/constants/colors.dart';
+import 'package:shopping_app/utils/constants/sizes.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
 class Store extends StatelessWidget {
   const Store({super.key});
@@ -18,6 +21,37 @@ class Store extends StatelessWidget {
           ),
         ],
       ),
+      body: NestedScrollView(
+          headerSliverBuilder: (_, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                floating: true,
+                backgroundColor: THelperFunctions.isDarkMode(context)
+                    ? TColors.black
+                    : TColors.white,
+                expandedHeight: 440,
+                flexibleSpace: Padding(
+                  padding: EdgeInsets.all(TSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: const [
+                      SizedBox(height: TSizes.spaceBtwItems),
+                      TSearchContainer(
+                          text: '',
+                          showBorder: true,
+                          showBackground: false,
+                          padding: EdgeInsets.zero),
+                      SizedBox(height: TSizes.spaceBtwSections),
+                    ],
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: Container()),
     );
   }
 }
