73a7c556903cf03d8b42c2da5c1e8c83ed482b33
chandan <chandanmahato624@gmail.com>
2024-03-09 22:19:29 +0530

    Now Completed Grid View and today task

--- shopping_app/lib/features/shop/screens/store/store.dart
+++ shopping_app/lib/features/shop/screens/store/store.dart
@@ -59,6 +59,7 @@ class Store extends StatelessWidget {
                       const SizedBox(height: TSizes.spaceBtwItems / 1.5),
                       TRridLayout(
                           itemCount: 4,
+                          mainAxisExtent: 80,
                           itemBuilder: (_, index) {
                             return GestureDetector(
                               onTap: () {},
@@ -69,32 +70,37 @@ class Store extends StatelessWidget {
                                 child: Row(
                                   children: [
                                     /// -- Icon
-                                    TCircularImage(
-                                      isNetworkImage: false,
-                                      image: TImages.clothIcon,
-                                      backgroundColor: Colors.transparent,
-                                      overlayColor:
-                                          THelperFunctions.isDarkMode(context)
-                                              ? TColors.white
-                                              : TColors.black,
+                                    Flexible(
+                                      child: TCircularImage(
+                                        isNetworkImage: false,
+                                        image: TImages.clothIcon,
+                                        backgroundColor: Colors.transparent,
+                                        overlayColor:
+                                            THelperFunctions.isDarkMode(context)
+                                                ? TColors.white
+                                                : TColors.black,
+                                      ),
                                     ),
                                     const SizedBox(
                                         height: TSizes.spaceBtwItems / 2),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        const TBrandTitleWithVerifiedIcon(
-                                            title: "Nike",
-                                            brandTextSize: TextSizes.large),
-                                        Text(
-                                          '256 Products',
-                                          overflow: TextOverflow.ellipsis,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .labelMedium,
-                                        )
-                                      ],
+                                    Expanded(
+                                      child: Column(
+                                        mainAxisSize: MainAxisSize.min,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          const TBrandTitleWithVerifiedIcon(
+                                              title: "Nike",
+                                              brandTextSize: TextSizes.large),
+                                          Text(
+                                            '256 Products',
+                                            overflow: TextOverflow.ellipsis,
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .labelMedium,
+                                          )
+                                        ],
+                                      ),
                                     )
                                   ],
                                 ),
