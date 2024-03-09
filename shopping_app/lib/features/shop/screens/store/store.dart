b61f2cd0e4c552ac9d51f6ff5422389519850495
chandan <chandanmahato624@gmail.com>
2024-03-09 11:45:26 +0530

    Working On circular image file

--- shopping_app/lib/features/shop/screens/store/store.dart
+++ shopping_app/lib/features/shop/screens/store/store.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/common/widgets/appbar/appbar.dart';
+import 'package:shopping_app/common/widgets/custom_shapes/container/rounded_container.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/container/search_container.dart';
 import 'package:shopping_app/common/widgets/products.cart/cart_menu_icon.dart';
+import 'package:shopping_app/common/widgets/texts/section_heading.dart';
 import 'package:shopping_app/utils/constants/colors.dart';
+import 'package:shopping_app/utils/constants/image_strings.dart';
 import 'package:shopping_app/utils/constants/sizes.dart';
 import 'package:shopping_app/utils/helpers/helper_functions.dart';
 
@@ -33,18 +36,35 @@ class Store extends StatelessWidget {
                     : TColors.white,
                 expandedHeight: 440,
                 flexibleSpace: Padding(
-                  padding: EdgeInsets.all(TSizes.defaultSpace),
+                  padding: const EdgeInsets.all(TSizes.defaultSpace),
                   child: ListView(
                     shrinkWrap: true,
                     physics: const NeverScrollableScrollPhysics(),
-                    children: const [
-                      SizedBox(height: TSizes.spaceBtwItems),
-                      TSearchContainer(
+                    children: [
+                      const SizedBox(height: TSizes.spaceBtwItems),
+                      const TSearchContainer(
                           text: '',
                           showBorder: true,
                           showBackground: false,
                           padding: EdgeInsets.zero),
-                      SizedBox(height: TSizes.spaceBtwSections),
+                      const SizedBox(height: TSizes.spaceBtwSections),
+
+                      /// -- Featured Brands
+                      TSectionHeading(
+                          title: "Featured Brands", onPressed: () {}),
+                      const SizedBox(height: TSizes.spaceBtwItems / 1.5),
+
+                      TRoundedContainer(
+                        padding: const EdgeInsets.all(TSizes.sm),
+                        showBorder: true,
+                        backgroundColor: Colors.transparent,
+                        child: Row(
+                          children: [
+                            /// -- Icon
+                            TCircularImage(),
+                          ],
+                        ),
+                      )
                     ],
                   ),
                 ),
