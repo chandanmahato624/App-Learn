eaf0d0a636e19e42c35b5fa74c2942bf9c794315
chandan <chandanmahato624@gmail.com>
2024-03-16 14:32:42 +0530

    9.13 veriations descriptions

--- shopping_app/lib/features/personalization/screens/product_details/product_attributes.dart
+++ shopping_app/lib/features/personalization/screens/product_details/product_attributes.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:shopping_app/common/widgets/products/product_cards/product_price_text.dart';
+import 'package:shopping_app/common/widgets/texts/product_title_text.dart';
 import 'package:shopping_app/common/widgets/texts/section_heading.dart';
 import 'package:shopping_app/utils/constants/colors.dart';
 import 'package:shopping_app/utils/constants/sizes.dart';
@@ -16,17 +18,50 @@ class TProductAttributes extends StatelessWidget {
       children: [
         /// selected attribute pricing & description
         TRoundedContainer(
+          padding: const EdgeInsets.all(TSizes.md),
           backgroundColor: dark ? TColors.darkGrey : TColors.grey,
-          child: const Column(
+          child: Column(
             children: [
               Row(
                 children: [
-                  TSectionHeading(title: 'Variation', showActionButton: false),
-                  SizedBox(
-                    width: TSizes.spaceBtwItems,
-                  )
+                  const TSectionHeading(
+                      title: 'Variation', showActionButton: false),
+                  const SizedBox(width: TSizes.spaceBtwItems),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          ///Acual price
+                          const TProductTitleText(
+                              title: 'Price', smallSize: true),
+                          Text(
+                            '\$25',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleSmall!
+                                .apply(decoration: TextDecoration.lineThrough),
+                          ),
+                          const SizedBox(width: TSizes.spaceBtwItems),
+                          const TProdutPriceText(price: '20'),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const TProductTitleText(
+                              title: 'stock :', smallSize: true),
+                          Text(
+                            'in Stock',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ],
-              )
+              ),
+
+              /// Varition secriptions
             ],
           ),
         )
