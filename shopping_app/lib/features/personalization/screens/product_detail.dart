9a51228dd4d7b629d1f2bcdd7efe950705ca04b8
chandan <chandanmahato624@gmail.com>
2024-03-18 10:06:46 +0530

    bottom navigations done

--- shopping_app/lib/features/personalization/screens/product_detail.dart
+++ shopping_app/lib/features/personalization/screens/product_detail.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:readmore/readmore.dart';
 import 'package:shopping_app/common/widgets/appbar/appbar.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/container/rounded_container.dart';
 import 'package:shopping_app/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:shopping_app/common/widgets/icons/t_circular_icon.dart';
 import 'package:shopping_app/common/widgets/images/t_rounded_image.dart';
+import 'package:shopping_app/common/widgets/texts/section_heading.dart';
+import 'package:shopping_app/features/personalization/screens/product_details/bottom_add_tocart_widget.dart';
 import 'package:shopping_app/features/personalization/screens/product_details/product_attributes.dart';
 import 'package:shopping_app/features/personalization/screens/product_details/product_details_image_slider.dart';
 import 'package:shopping_app/features/personalization/screens/product_details/product_meta_data.dart';
@@ -22,6 +25,7 @@ class ProductDetailScreen extends StatelessWidget {
     final dark = THelperFunctions.isDarkMode(context);
 
     return Scaffold(
+      bottomNavigationBar: TBottomAddToCart(),
       body: SingleChildScrollView(
         child: Column(
           children: [
@@ -61,6 +65,42 @@ class ProductDetailScreen extends StatelessWidget {
                   ),
 
                   /// Descriptions - 26.28
+                  const TSectionHeading(
+                      title: 'Descriptions', showActionButton: false),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  const ReadMoreText(
+                    'A t-shirt, a staple of casual wear, is a versatile garment adorning torsos worldwide. Its simplicity belies its impact, serving as a canvas for self-expression, from graphic designs to witty slogans. Soft cotton embraces skin, offering comfort in various styles and fits. A fashion essential, the humble tee transcends trends, enduring through generations.',
+                    trimLines: 2,
+                    trimMode: TrimMode.Line,
+                    trimCollapsedText: 'Show more',
+                    trimExpandedText: 'less',
+                    moreStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                    lessStyle:
+                        TextStyle(fontSize: 14, fontWeight: FontWeight.w800),
+                  ),
+
+                  /// Reviews
+                  const Divider(),
+                  const SizedBox(height: TSizes.spaceBtwItems),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const TSectionHeading(
+                        title: 'Reviews(199)',
+                        showActionButton: false,
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Iconsax.arrow_right_3,
+                            size: 18,
+                          )),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: TSizes.spaceBtwSections,
+                  )
                 ],
               ),
             )
