c2e7c215856387bd2f321638b373f39e68405b89
chandan <chandanmahato624@gmail.com>
2024-03-25 19:12:59 +0530

    Auth file created need to cganges

---
+++ shopping_app/lib/data/repositories/authentication_repo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_instance/get_instance.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get_storage/get_storage.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  /// Variables
+  final deviceStorage = GetStorage();
+
+  /// Called from main.dart
+  @override
+  void onReady() {
+    FlutterNativeSplash.remove();
+    screenRedirect();
+  }
+
+  /// Funcation to show relevent screen
+  screenRedirect() async {}
+}
