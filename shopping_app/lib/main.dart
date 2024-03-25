119e852efeec602e684fe3f2af9c4ff98213eb70
chandan <chandanmahato624@gmail.com>
2024-03-25 08:27:43 +0530

    Firebase setup code added

--- shopping_app/lib/main.dart
+++ shopping_app/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  // Todo: initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const App());
 }
