10a94623775e1f71fba7599c88eb4beb7ceebc19
chandan <chandanmahato624@gmail.com>
2024-04-02 11:50:28 +0530

    sign_up controller stoped for temp.

--- shopping_app/lib/features/authentication/controllers/signup/signup_controller.dart
+++ shopping_app/lib/features/authentication/controllers/signup/signup_controller.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:shopping_app/features/authentication/Networks/network_manager.dart';
+import 'package:shopping_app/utils/constants/image_strings.dart';
+import 'package:shopping_app/utils/popups/full_screen_loader.dart';
 
 class SingUpController extends GetxController {
   static SingUpController get instance => Get.find();
@@ -17,7 +20,26 @@ class SingUpController extends GetxController {
 
   /// SIGNUP
   Future<void> singup() async {
-    try {} catch (e) {
-    } finally {}
+    try {
+      //Start loading
+      TFullScreenLoader.openLoadingDialog(
+          'We are processing your information...', TImages.verifyIllustration);
+      // check internet connectivity
+      final isConnected = await NetworkManager.instance.isConnected();
+      if (isConnected) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+      // Form validation
+      if (!signupFormKey.currentState!.validate()) {
+        TFullScreenLoader.stopLoading();
+        return;
+      }
+    } catch (e) {
+      // ---------------------18.30
+      // Tloders.errorSnackbar(title: 'On Snap!', message: e.toString());
+    } finally {
+      TFullScreenLoader.stopLoading();
+    }
   }
 }
