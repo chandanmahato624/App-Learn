abd3b45910592e80a1c23e9238e8224ce0ed20f0
chandan <chandanmahato624@gmail.com>
2024-04-06 08:05:58 +0530

    checkout screen 10.20 done

--- shopping_app/lib/features/shop/screens/cart/cart.dart
+++ shopping_app/lib/features/shop/screens/cart/cart.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:shopping_app/common/widgets/appbar/appbar.dart';
 import 'package:shopping_app/common/widgets/icons/t_circular_icon.dart';
@@ -9,6 +11,7 @@ import 'package:shopping_app/common/widgets/texts/product_title_text.dart';
 import 'package:shopping_app/common/widgets/texts/t_brand_title_with_verified_icon.dart';
 import 'package:shopping_app/features/shop/screens/cart/cart_item.dart';
 import 'package:shopping_app/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:shopping_app/features/shop/screens/checkout/checkout.dart';
 import 'package:shopping_app/utils/constants/colors.dart';
 import 'package:shopping_app/utils/constants/image_strings.dart';
 import 'package:shopping_app/utils/constants/sizes.dart';
@@ -33,7 +36,8 @@ class CartScreen extends StatelessWidget {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(TSizes.defaultSpace),
         child: ElevatedButton(
-            onPressed: () {}, child: const Text('Checkout \$624.0')),
+            onPressed: () => Get.to(() => const CheckoutScreen()),
+            child: const Text('Checkout \$624.0')),
       ),
     );
   }
--- shopping_app/lib/features/shop/screens/checkout/checkout.dart
+++ shopping_app/lib/features/shop/screens/checkout/checkout.dart
@@ -1,1 +1,64 @@
-///-------------5.11
+import 'package:flutter/material.dart';
+import 'package:shopping_app/common/widgets/appbar/appbar.dart';
+import 'package:shopping_app/common/widgets/custom_shapes/container/rounded_container.dart';
+import 'package:shopping_app/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:shopping_app/utils/constants/colors.dart';
+import 'package:shopping_app/utils/constants/sizes.dart';
+import 'package:shopping_app/utils/helpers/helper_functions.dart';
+
+class CheckoutScreen extends StatelessWidget {
+  const CheckoutScreen({Key? Key}) : super(key: Key);
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      appBar: TAppBar(
+        showBackArrow: true,
+        title: Text('Order Review',
+            style: Theme.of(context).textTheme.headlineSmall),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Items in cart
+              TCartItems(showAddRemoveButtons: false),
+              SizedBox(height: TSizes.spaceBtwSections),
+
+              /// coupon section
+              TRoundedContainer(
+                showBorder: true,
+                backgroundColor: dark ? TColors.dark : TColors.white,
+                padding: const EdgeInsets.only(
+                    top: TSizes.sm,
+                    bottom: TSizes.sm,
+                    right: TSizes.sm,
+                    left: TSizes.md),
+                child: Row(
+                  children: [
+                    /// Textfild
+                    Flexible(
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "have a promocode",
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: TSizes.spaceBtwSections),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
