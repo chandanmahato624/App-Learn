e7098085d6d01e9f88297d695f5add9a862f1810
chandan <chandanmahato624@gmail.com>
2024-04-16 10:21:23 +0530

    verify controller created

---
+++ shopping_app/lib/features/authentication/controllers/signup/verify_email_controller.dart
@@ -0,0 +1,16 @@
+import 'package:get/get.dart';
+
+class VerifyEmailController extends GetxController {
+  static VerifyEmailController get instance => Get.find();
+
+  /// Send Email whenever Verified Screen appearrs & Set timer for auto redirect
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  /// Send Email verification link
+  /// Timer to automatically redirect on email verification
+  /// Manual check if email Vrified
+  /// 4.51
+}
