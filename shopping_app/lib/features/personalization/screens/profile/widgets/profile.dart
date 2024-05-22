ece373a848a5fb3337cabb819b9fa98eedd4c21b
chandan <chandanmahato624@gmail.com>
2024-05-22 18:09:53 +0530

    Commit prevoius code

--- shopping_app/lib/features/personalization/screens/profile/widgets/profile.dart
+++ shopping_app/lib/features/personalization/screens/profile/widgets/profile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/common/widgets/appbar/appbar.dart';
 import 'package:shopping_app/common/widgets/images/t_circular_image.dart';
 import 'package:shopping_app/common/widgets/texts/section_heading.dart';
+import 'package:shopping_app/features/personalization/controllers/user_controller.dart';
 import 'package:shopping_app/features/personalization/screens/profile/widgets/profile_menu.dart';
 import 'package:shopping_app/utils/constants/image_strings.dart';
 import 'package:shopping_app/utils/constants/sizes.dart';
@@ -11,6 +12,8 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = UserController.instance;
+    //24.11
     return Scaffold(
       appBar: const TAppBar(showBackArrow: true, title: Text('Profile')),
       body: SingleChildScrollView(
@@ -42,9 +45,13 @@ class ProfileScreen extends StatelessWidget {
               const SizedBox(height: TSizes.spaceBtwItems),
 
               TProfileMenu(
-                  title: 'Name', value: 'Chandan mahato', onPressed: () {}),
+                  title: 'Name',
+                  value: controller.user.value.fullName,
+                  onPressed: () {}),
               TProfileMenu(
-                  title: 'Username', value: 'chandan_mahato', onPressed: () {}),
+                  title: 'Username',
+                  value: controller.user.value.username,
+                  onPressed: () {}),
 
               const SizedBox(height: TSizes.spaceBtwItems / 2),
               const Divider(),
@@ -55,17 +62,22 @@ class ProfileScreen extends StatelessWidget {
                   title: 'Personal information', showActionButton: false),
               const SizedBox(height: TSizes.spaceBtwItems),
               TProfileMenu(
-                  title: 'Name', value: 'Chandan mahato', onPressed: () {}),
+                  title: 'User Id',
+                  value: controller.user.value.id,
+                  onPressed: () {}),
               TProfileMenu(
-                  title: 'Username', value: 'chandan_mahato', onPressed: () {}),
+                  title: 'E-mail',
+                  value: controller.user.value.email,
+                  onPressed: () {}),
               TProfileMenu(
-                  title: 'Name', value: 'Chandan mahato', onPressed: () {}),
+                  title: 'Phone Number',
+                  value: controller.user.value.phoneNumber,
+                  onPressed: () {}),
+              TProfileMenu(title: 'Gender', value: 'Male', onPressed: () {}),
               TProfileMenu(
-                  title: 'Username', value: 'chandan_mahato', onPressed: () {}),
-              TProfileMenu(
-                  title: 'Name', value: 'Chandan mahato', onPressed: () {}),
-              TProfileMenu(
-                  title: 'Username', value: 'chandan_mahato', onPressed: () {}),
+                  title: 'Date of Birth',
+                  value: '02-05-2000',
+                  onPressed: () {}),
 
               /// close account
               const Divider(),
